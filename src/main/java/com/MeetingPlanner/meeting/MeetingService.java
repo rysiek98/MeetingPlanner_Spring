50ed8c8ad0fa77611f270205f5cc25c66f7eb455
rysiek98 <52510889+rysiek98@users.noreply.github.com>
2020-08-02 17:41:36 +0200

    working H2

--- src/main/java/com/MeetingPlanner/meeting/MeetingService.java
+++ src/main/java/com/MeetingPlanner/meeting/MeetingService.java
@@ -1,6 +1,4 @@
 package com.MeetingPlanner.meeting;
-import com.MeetingPlanner.calendar.Calendar;
-import com.MeetingPlanner.calendar.CalendarRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
