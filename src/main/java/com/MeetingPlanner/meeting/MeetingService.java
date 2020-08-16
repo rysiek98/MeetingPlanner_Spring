857efb207541ce9e51062f35920c94c3718fa901
rysiek98 <52510889+rysiek98@users.noreply.github.com>
2020-08-16 22:22:40 +0200

    Clean code

--- src/main/java/com/MeetingPlanner/meeting/MeetingService.java
+++ src/main/java/com/MeetingPlanner/meeting/MeetingService.java
@@ -3,13 +3,11 @@ package com.MeetingPlanner.meeting;
 import com.MeetingPlanner.calendar.Calendar;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.function.Supplier;
-
+import java.util.stream.Collectors;
 import static java.util.Collections.sort;
 
 
@@ -19,80 +17,82 @@ public class MeetingService {
 
     private final MeetingRepository meetingRepository;
 
-
-    public static void updateMeetings(List<Meeting> meetings,List<Meeting> updateMeetings) {
-
+    public static void removeMeetings(List<Meeting> meetings,List<Meeting> updateMeetings){
         Collections.reverse(meetings);
         meetings.removeIf(meeting -> meetings.size() > updateMeetings.size());
         Collections.reverse(meetings);
+    }
 
+    public static void updateMeetings(List<Meeting> meetings,List<Meeting> updateMeetings) {
 
-        for(int i = 0; i < updateMeetings.size(); i++){
-            if(i<meetings.size()) {
-                meetings.get(i).setStartTime(updateMeetings.get(i).getStartTime());
-                meetings.get(i).setEndTime(updateMeetings.get(i).getEndTime());
-            }else {
-                meetings.add(updateMeetings.get(i));
+        removeMeetings(meetings, updateMeetings);
+
+        updateMeetings.stream().forEach(meeting -> {
+            if (updateMeetings.indexOf(meeting) < meetings.size()) {
+                meetings.get(updateMeetings.indexOf(meeting)).setStartTime(meeting.getStartTime());
+                meetings.get(updateMeetings.indexOf(meeting)).setEndTime(meeting.getEndTime());
+            } else {
+                meetings.add(meeting);
             }
-            meetings.get(i).countDuration();
+            meetings.get(updateMeetings.indexOf(meeting)).countDuration();
+        });
+
+    }
+
+    public static boolean newMeetingCalculateCondition(Meeting meeting
+            ,Meeting meetingCalendar, int meetingDuration){
+
+        LocalTime newMeetingEndTime = meeting.getEndTime().minusMinutes(meetingDuration);
+
+        if(newMeetingEndTime.isAfter(meetingCalendar.getStartTime())
+                || newMeetingEndTime.equals(meetingCalendar.getStartTime())){
+            return true;
         }
+        else return false;
     }
 
     public static List<Meeting> newMeetingCalculate(Calendar calendar1, Calendar calendar2, int meetingDuration){
 
         List<Meeting> possibleMeetingTime = new ArrayList<>();
-        List<Meeting> calendar1Meetings = countFreeTime(calendar1);
-        List<Meeting> calendar2Meetings = countFreeTime(calendar2);
-        LocalTime startTime, endTime;
         int iterator = 1;
 
-        for (Meeting meeting : calendar1Meetings) {
-            for (Meeting value : calendar2Meetings) {
-                if (meeting.getStartTime().isAfter(value.getStartTime()) || meeting.getStartTime().equals(value.getStartTime())) {
-                    startTime = meeting.getStartTime();
-                    endTime = meeting.getStartTime().plusMinutes(meetingDuration);
+        List<Meeting> calendar1Meetings = countFreeTime(calendar1).stream()
+                .filter(meeting -> meeting.getDuration() >= meetingDuration).collect(Collectors.toList());
 
-                    boolean flag = false;
-                    while (condition1(meeting, value, endTime)) {
-                        endTime = endTime.plusMinutes(meetingDuration);
-                        flag = true;
-                    }
-                    if (flag) {
-                        possibleMeetingTime.add(new Meeting(iterator,startTime, endTime.minusMinutes(meetingDuration)));
+        List<Meeting> calendar2Meetings = countFreeTime(calendar2).stream()
+                .filter(meeting -> meeting.getDuration() >= meetingDuration).collect(Collectors.toList());
+
+
+        for(Meeting meetingCalendar1 : calendar1Meetings) {
+            for (Meeting meetingCalendar2 : calendar2Meetings) {
+
+                if(meetingCalendar1.getStartTime().isBefore(meetingCalendar2.getStartTime())
+                        || meetingCalendar1.getStartTime().equals(meetingCalendar2.getStartTime())){
+
+                    if(newMeetingCalculateCondition(meetingCalendar1
+                            ,meetingCalendar2 ,meetingDuration)){
+
+                        possibleMeetingTime.add(new Meeting(
+                                iterator
+                                ,meetingCalendar2.getStartTime()
+                                ,meetingCalendar1.getEndTime()));
                         iterator++;
                     }
+                }else {
+                    if(newMeetingCalculateCondition(meetingCalendar2
+                            ,meetingCalendar1 ,meetingDuration)){
 
-                } else {
-                    startTime = value.getStartTime();
-                    endTime = value.getStartTime().plusMinutes(meetingDuration);
-                    boolean flag = false;
-
-                    while (condition2(meeting, value, endTime)) {
-                        endTime = endTime.plusMinutes(meetingDuration);
-                        flag = true;
-                    }
-                    if (flag) {
-                        possibleMeetingTime.add(new Meeting(iterator,startTime, endTime.minusMinutes(meetingDuration)));
+                        possibleMeetingTime.add(new Meeting(
+                                iterator
+                                ,meetingCalendar1.getStartTime()
+                                ,meetingCalendar2.getEndTime()));
                         iterator++;
                     }
                 }
             }
         }
+
         return possibleMeetingTime;
-    }
-
-    public static boolean condition1(Meeting meeting, Meeting value, LocalTime endTime){
-        return (endTime.isBefore(meeting.getEndTime()) && endTime.isBefore(value.getEndTime()) && endTime.isAfter(value.getStartTime()))
-                || (endTime.equals(meeting.getEndTime()) && endTime.isBefore(value.getEndTime()) && endTime.isAfter(value.getStartTime()))
-                || (endTime.isBefore(meeting.getEndTime()) && endTime.equals(value.getEndTime()) && endTime.isAfter(value.getStartTime()))
-                || (endTime.equals(meeting.getEndTime()) && endTime.equals(value.getEndTime()) && endTime.isAfter(value.getStartTime()));
-    }
-
-    public static boolean condition2(Meeting meeting, Meeting value, LocalTime endTime){
-        return (endTime.isBefore(meeting.getEndTime()) && endTime.isBefore(value.getEndTime()) && endTime.isAfter(meeting.getStartTime()))
-                || (endTime.equals(meeting.getEndTime()) && endTime.isBefore(value.getEndTime()) && endTime.isAfter(meeting.getStartTime()))
-                || (endTime.isBefore(meeting.getEndTime()) && endTime.equals(value.getEndTime()) && endTime.isAfter(meeting.getStartTime()))
-                || (endTime.equals(meeting.getEndTime()) && endTime.equals(value.getEndTime()) && endTime.isAfter(meeting.getStartTime()));
     }
 
     public static List<Meeting> countFreeTime(Calendar calendar){
@@ -101,6 +101,7 @@ public class MeetingService {
         List<Meeting> freeTime = new ArrayList<>();
 
         sort(meetings);
+
         if(meetings.size() == 0){
             freeTime.add(new Meeting(calendar.getWorkBegin(), calendar.getWorkEnd()));
         }else{
@@ -113,6 +114,7 @@ public class MeetingService {
                 if(!meetings.get(i).getEndTime().equals(meetings.get(i+1).getStartTime())){
                     freeTime.add(new Meeting(meetings.get(i).getEndTime(), meetings.get(i+1).getStartTime()));
                 }
+                
             }
 
             if(!calendar.getWorkEnd().equals(meetings.get(meetings.size()-1).getEndTime())){
@@ -123,6 +125,7 @@ public class MeetingService {
     }
 
     public static List<Meeting> newMeetingTime(Calendar calendar1, Calendar calendar2, int meetingDuration){
+
         if(calendar1.getData().equals(calendar2.getData())){
             return newMeetingCalculate(calendar1,calendar2,meetingDuration);
         }else return new ArrayList<>();
